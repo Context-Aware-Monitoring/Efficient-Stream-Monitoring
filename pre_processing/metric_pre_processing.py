d006b985129d72029dc18490f7955c39ea49dfc9
Tom George <utduu@student.kit.edu>
2021-01-04 12:13:15 +0100

    Add functionality to linear interpolate metric data
    Relates to #7

--- pre_processing/metric_pre_processing.py
+++ pre_processing/metric_pre_processing.py
@@ -1,13 +1,111 @@
 """This module performs various pre processing operations on the metric data."""
 import csv
+from datetime import datetime, timedelta
+import pdb
 
-POS_TIMESTAMP = 0
+POS_TIMESTAMP = 0 
 
+def fill_metrics_missing_seconds_using_linear_interpolation(metrics_csv_file_path):
+    """This method fills the missing second intervals in the provided csv file by performing linear
+    interpolation. It writes a new csv file at the same location with the suffix '_filled'.
+
+    Args:
+      metrics_csv_file_path: The path to the csv file containing the metrics data.
+    Returns:
+      None
+    """
+    rows = list()
+    with open(metrics_csv_file_path) as metrics_csv_file:
+        csv_reader = csv.reader(metrics_csv_file, delimiter=',')
+        header = None
+        for row in csv_reader:
+            if header == None:
+                header = row
+                continue
+            if len(rows) == 0:
+                rows.append(row)
+            else:
+                prev_timestamp = _convert_timestamp_string_to_datetime(rows[-1][POS_TIMESTAMP])
+                current_timestamp = _convert_timestamp_string_to_datetime(row[POS_TIMESTAMP])
+
+                if (prev_timestamp + timedelta(seconds=1)) == current_timestamp:
+                    rows.append(row)
+                else:
+                    rows.extend(list(_get_interpolated_rows(rows[-1], row)))
+                    rows.append(row)
+
+    output_file_name = _get_output_file_name_with_suffix(metrics_csv_file_path, '_filled')
+    with open(output_file_name, 'w') as output_file:
+        output_file.write(','.join(header))
+        output_file.write('\n')
+        for row in rows:
+            output_file.write(','.join(row))
+            output_file.write('\n')
+        
+
+
+def _get_interpolated_rows(row1, row2):
+    """Performs linear interpolation for the values inside the passed rows.
+
+    Args:
+      row1 (array): Array with values containing the earlier timestamp.
+      row2 (array): Array with values containing the later timestamp.
+
+    Returns:
+      Array of arrays of size n, where n is the number of seconds between the two timestamps. Each array contains the interpolated values for a timestamp.
+    """
+    ts1 = _convert_timestamp_string_to_datetime(row1[POS_TIMESTAMP])
+    ts2 = _convert_timestamp_string_to_datetime(row2[POS_TIMESTAMP])
+    diff_seconds = (ts2 - ts1).seconds
+
+    interpolated_rows = []
+    slope = [0] * len(row1)
+
+    for i,_ in enumerate(slope):
+        if i == POS_TIMESTAMP:
+            continue
+        
+        diff_values = float(row2[i]) - float(row1[i])
+        slope[i] = round(diff_values / diff_seconds, 2)
+
+    for s in range(1, diff_seconds):
+        interpolated_rows.append([0] * len(row1))
+        
+        for i,_ in enumerate(row1):
+            if i == POS_TIMESTAMP:
+                interpolated_rows[-1][i] = _convert_datetime_to_string_timestamp(ts1 + timedelta(seconds=s))
+                continue
+            
+            interpolated_rows[-1][i] = str(round((float(row1[i]) + slope[i] * s), 2))
+
+    return interpolated_rows
+            
+
+def _convert_datetime_to_string_timestamp(datetime):
+    """Converts the passed datetime to a string.
+
+    Args:
+      datetime (datetime): Timestamp as datetime object.
+    Returns:
+      String representation of the datetime object.
+    """
+    return datetime.strftime('%Y-%m-%d %H:%M:%S') + ' CEST'
+    
+def _convert_timestamp_string_to_datetime(timestamp):
+    """Converts the passed string to a datetime object.
+    
+    Args:
+      timestamp (string): Timestamp as string
+    Returns:
+      Timestamp as datetime objects.
+    """
+    timestamp = timestamp[:-5] # remove the ' CEST'
+    return datetime.strptime(timestamp, '%Y-%m-%d %H:%M:%S')
 
 def get_metrics_on_seconds_interval(metrics_csv_file_path):
     """This method turns the metrics in the csv_file into on that has seconds intervals between the
     different measurements. To do this it averages all the measurements taken in a second. It will
-    write a new csv_file at the same location with the suffix seconds.
+    write a new csv file at the same location with the suffix '_seconds'.
 
     Args:
       metrics_csv_file_path: The path to the csv file containing the metrics data.
@@ -41,7 +139,7 @@ def get_metrics_on_seconds_interval(metrics_csv_file_path):
 
         output_lines.append(_get_average_for_row(aggregated_values, values_per_timestamp))
 
-        output_file_name = _get_output_file_name(metrics_csv_file_path)
+        output_file_name = _get_output_file_name_with_suffix(metrics_csv_file_path, '_seconds')
         with open(output_file_name, 'w') as output_file:
             for line in output_lines:
                 output_file.write(','.join(line))
@@ -93,15 +191,15 @@ def _get_average_for_row(row, divisor):
     return output
 
 
-def _get_output_file_name(csv_file_name):
+def _get_output_file_name_with_suffix(csv_file_name, suffix):
     """This method returns for a path of a csv file the same path but with a filename that is
-    suffixed by _seconds.
+    suffixed by the passed suffix.
 
     Args:
       csv_file_name: Path to a csv file
 
     Returns:
-      The input path where the filename is suffixed by _seconds.
+      The input path where the filename is suffixed by the passed suffix.
     """
     assert csv_file_name[-4:] == '.csv'
-    return csv_file_name[:-4] + '_seconds.csv'
+    return csv_file_name[:-4] + suffix + '.csv'
--- tests/test_metric_pre_processing.py
+++ tests/test_metric_pre_processing.py
@@ -2,6 +2,7 @@ import unittest
 import os
 import sys
 import filecmp
+import pdb
 
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 from pre_processing import metric_pre_processing as mpp
@@ -40,20 +41,48 @@ class MetricPreProcessingTest(unittest.TestCase):
 
         self.assertEqual(expected_result, mpp._get_average_for_row(row, 1))
 
-    def test_get_output_file_name(self):
-        self.assertEqual('./test_seconds.csv', mpp._get_output_file_name('./test.csv'))
-        self.assertEqual('/this/is/a/absolute/path/to/a/file_seconds.csv',
-                         mpp._get_output_file_name('/this/is/a/absolute/path/to/a/file.csv'))
+    def test_get_output_file_name_with_suffix(self):
+        self.assertEqual('./test_suffix.csv', mpp._get_output_file_name_with_suffix('./test.csv', '_suffix'))
+        self.assertEqual('/this/is/a/absolute/path/to/a/file_suffix.csv',
+                         mpp._get_output_file_name_with_suffix('/this/is/a/absolute/path/to/a/file.csv', '_suffix'))
 
+
+    def test_get_interpolated_rows_1_second_gap(self):
+        row1 = ['2019-11-19 16:56:32 CEST','12','10000000000','8','0.8','1.02','1.18']
+        row2 = ['2019-11-19 16:56:34 CEST','10','11000000000','8','0.8','1.00','1.20']
+
+        expected = [['2019-11-19 16:56:33 CEST', '11.0', '10500000000.0', '8.0', '0.8', '1.01', '1.19']]
+        actual = mpp._get_interpolated_rows(row1, row2)
+
+        self.assertEqual(expected, actual)
+
+    def test_get_interpolated_rows_2_second_gap(self):
+        row1 = ['2019-11-19 16:56:32 CEST','12','10000000000','8','0.8','1.02','1.18']
+        row2 = ['2019-11-19 16:56:35 CEST','9','11500000000','8','1.1','1.02','1.21']
+
+        expected = [
+            ['2019-11-19 16:56:33 CEST', '11.0', '10500000000.0', '8.0', '0.9', '1.02', '1.19'],
+            ['2019-11-19 16:56:34 CEST', '10.0', '11000000000.0', '8.0', '1.0', '1.02', '1.20']            
+        ]
+        actual = mpp._get_interpolated_rows(row1, row2)
+
+        
     def test_get_metrics_on_seconds_interval(self):
         mpp.get_metrics_on_seconds_interval('./data/test_metrics.csv')
 
         output_file_written = os.path.exists('./data/test_metrics_seconds.csv')
 
         self.assertTrue(output_file_written)
-        self.assertTrue(filecmp.cmp('./data/test_metrics_seconds.csv', './data/test_metrics_expected.csv'))
-        
+        self.assertTrue(filecmp.cmp('./data/test_metrics_seconds.csv', './data/test_metrics_seconds_expected.csv'))
         os.remove('./data/test_metrics_seconds.csv')
+        
+    def test_fill_metrics_missing_seconds_using_linear_interpolation(self):
+        mpp.fill_metrics_missing_seconds_using_linear_interpolation('./data/test_metrics2.csv')
+
+        output_file_written = os.path.exists('./data/test_metrics2_filled.csv')
+        self.assertTrue(output_file_written)
+        self.assertTrue(filecmp.cmp('./data/test_metrics2_filled.csv', './data/test_metrics2_expected.csv'))
+        # os.remove('./data/test_metrics_seconds.csv')
 
 if __name__ == '__main__':
     unittest.main()
