3d261d6e9ac8514d30065f0a5a18b5404344ba52
Tom George <17514362+Asupemuli473@users.noreply.github.com>
2021-06-02 21:43:54 +0200

    adjust time window of concurrent traces

--- src/global_config.py
+++ src/global_config.py
@@ -9,15 +9,16 @@ WINDOW_STEPS = [5, 10, 20]
 Ls = [5, 10, 20, 50, 100]
 THRESHOLDS = [0.6, 0.7, 0.8]
 REWARD_KINDS = ['top', 'threshold']
-SEQ = [True, False]
+SEQ = [False]
 
 DATA_DIR = '%s/data' % (dirname(dirname(abspath(__file__))))
 REWARDS_DIR = '%s/processed/rewards' % DATA_DIR
 EXPERIMENT_CONFIG_DIR = '%s/interim/experiment_configs' % DATA_DIR
+EXPERIMENT_SERIALIZATION_DIR = '%s/processed/experiment_results/' % DATA_DIR
 
 
 START_TRACES_SEQUENTIAL = np.datetime64('2019-11-19 17:38:39')
 END_TRACES_SEQUENTIAL = np.datetime64('2019-11-20 01:30:00')
 
-START_TRACES_CONCURRENT = np.datetime64('2019-11-19 15:12:13')
-END_TRACES_CONCURRENT = np.datetime64('2019-11-20 19:45:00')
+START_TRACES_CONCURRENT = np.datetime64('2019-11-25 15:12:13')
+END_TRACES_CONCURRENT = np.datetime64('2019-11-25 19:45:00')
