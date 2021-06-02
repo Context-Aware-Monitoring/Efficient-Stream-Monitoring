2e7e1d817ce60d79a02f699a769b05be8544c492
Tom George <17514362+Asupemuli473@users.noreply.github.com>
2021-06-02 21:46:33 +0200

    debug concurrent trace generation

--- src/features/build_features.py
+++ src/features/build_features.py
@@ -7,49 +7,42 @@ import itertools
 from . import context
 import global_config
 
-DATA_DIR = '%s/data' % dirname(dirname(dirname(abspath(__file__))))
 
-WINDOW_SIZES = [30,60]
-WINDOW_STEPS = [5,10,20]
+for seq, window_size, window_step in itertools.product(
+        global_config.SEQ,
+        global_config.WINDOW_SIZES,
+        global_config.WINDOW_STEPS
+):
+    seq_or_con = 'sequential_data' if seq else 'concurrent_data'
+    context.generate_context_csv(
+        context.HostExtractor(),
+        [
+            '%s/raw/%s/traces/boot_delete/' % (
+                global_config.DATA_DIR, seq_or_con),
+            '%s/raw/%s/traces/image_create_delete/' % (
+                global_config.DATA_DIR, seq_or_con),
+            '%s/raw/%s/traces/network_create_delete/' % (
+                global_config.DATA_DIR, seq_or_con)
+        ],
+        seq=seq,
+        window_size=window_size,
+        step=window_step,
+        outdir='%s/processed/context/' % global_config.DATA_DIR,
+        columns=['wally113', 'wally117', 'wally122', 'wally123', 'wally124']
+    )
 
-def _get_headers_from_csv_file(filepath):
-    headers = None
-    with open(filepath) as csvfile:
-        reader = csv.reader(csvfile)
-        headers = next(reader)
-
-    return headers
-
-
-if __name__ == '__main__':
-    for seq, window_size, window_step in itertools.product(
-            global_config.SEQ,
-            global_config.WINDOW_SIZES,
-            global_config.WINDOW_STEPS
-    ):
-        context.generate_context_csv(
-            context.HostExtractor(),
-            [
-                '%s/raw/sequential_data/traces/boot_delete/' % DATA_DIR,
-                '%s/raw/sequential_data/traces/image_create_delete/' % DATA_DIR,
-                '%s/raw/sequential_data/traces/network_create_delete/' % DATA_DIR
-            ],
-            seq=seq,
-            window_size=window_size,
-            step=window_step,
-            outdir='%s/processed/context/' % DATA_DIR,
-            columns=['wally113', 'wally117', 'wally122', 'wally123', 'wally124']
-        )
-        
-        context_filepath = context.generate_context_csv(
-            context.WorkloadExtractor(),
-            [
-                '%s/raw/sequential_data/traces/boot_delete/' % DATA_DIR,
-                '%s/raw/sequential_data/traces/image_create_delete/' % DATA_DIR,
-                '%s/raw/sequential_data/traces/network_create_delete/' % DATA_DIR
-            ],
-            seq=seq,
-            window_size=window_size,
-            step=window_step,
-            outdir='%s/processed/context/' % DATA_DIR
-        )
+    context_filepath = context.generate_context_csv(
+        context.WorkloadExtractor(),
+        [
+            '%s/raw/%s/traces/boot_delete/' % (
+                global_config.DATA_DIR, seq_or_con),
+            '%s/raw/%s/traces/image_create_delete/' % (
+                global_config.DATA_DIR, seq_or_con),
+            '%s/raw/%s/traces/network_create_delete/' % (
+                global_config.DATA_DIR, seq_or_con)
+        ],
+        seq=seq,
+        window_size=window_size,
+        step=window_step,
+        outdir='%s/processed/context/' % global_config.DATA_DIR
+    )
